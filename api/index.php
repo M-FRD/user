49584b80a348629712ab29c82b2942799f37659c
M-FRD <86618885+M-FRD@users.noreply.github.com>
2022-04-13 14:57:12 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/api/style.css">
+    <link rel="stylesheet" href="https://github.com/M-FRD/user/blob/main/api/style.css">
     <title>Générateur aléatoire</title>
 </head>
 <body>
